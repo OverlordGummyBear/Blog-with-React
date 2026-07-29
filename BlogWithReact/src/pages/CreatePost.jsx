82d1b879f8191d202fa5ed3ee46fa9c315e2e3f9
DANIELSTATIONÆR\danie <danielwlaursen@gmail.com>
2026-07-29 14:49:24 +0200

    Create layout for post creation page + apply styling

--- BlogWithReact/src/pages/CreatePost.jsx
+++ BlogWithReact/src/pages/CreatePost.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState, Fragment } from "react";
+import Header from "../components/Header";
 
-function CreatePost(){
-    return <h1> Hello this is to create a post </h1>
+function CreatePost(props){
+    const [note, setNote] = useState({title: "", description: ""});
+    const [isEditing, setEdit] = useState(props.editing);
+
+    return <Fragment>
+        <Header content="Create Blog Post"/> 
+        <div className="center-container">
+            <div className="post-creation-container">
+                <div>
+                    <form className="form-create-container">
+                        <input className="save-input blog-title" type="text" name="title" placeholder="Blog title" />
+
+                        <textarea className="save-input blog-description" name="description" placeholder="What is on your mind?" />
+                        <br/>
+                        <input className="submit-button name-btn" type="submit" value="Save" />
+                    </form>
+                </div>
+            </div>
+        </div>
+    </Fragment>
 }
 
 export default CreatePost;
